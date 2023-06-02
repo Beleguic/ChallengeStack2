f79b32731cb98e9a5308ec73647398f69b215c78
Thibault Beleguic <thibault.beleguic2@gmail.com>
2023-06-02 15:35:34 +0200

    add prefixe to conn flag

--- www/Views/Main/home.view.php
+++ www/Views/Main/home.view.php
@@ -6,9 +6,9 @@
 	//var_dump($this->data);
 	// Pour recupere la data passer par 'view->assign' -> '$this->data'
 
-	if(isset($_SESSION['login']['connected']) && $_SESSION['login']['connected']):
+	if(isset($_SESSION['zfgh_login']['connected']) && $_SESSION['zfgh_login']['connected']):
 		echo("<pre>");
-		var_dump($_SESSION['login']);
+		var_dump($_SESSION['zfgh_login']);
 		echo("</pre>");
 		?>
 		<a href="/logout">Deconnexion</a>
