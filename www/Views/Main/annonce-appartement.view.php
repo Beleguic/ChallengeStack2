38ab6e6d0b4e21676bc8145d6aab04a3ecbc5218
Sydney Nana Tchuigoua <sydneynt11@gmail.com>
2023-06-26 12:44:56 +0200

    feat(front annonce): Add home page annonces and details annonces

---
+++ www/Views/Main/annonce-appartement.view.php
@@ -0,0 +1,34 @@
+<div class="container">
+  <h1 class="text-center py-4">Annonce détaillée</h1>
+  <div class="row">
+    <div class="col-md-6">
+      <img src="../../asset/img/appartement.jpg" class="img-fluid rounded" alt="Appartement">
+    </div>
+    <div class="col-md-6">
+      <h2 class="fs-2 text-primary">Appartement</h2>
+      <h4 class="">Prix : 1249€ / mois</h4>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi labore totam, numquam ullam accusantium
+        cum odit rem saepe accusamus expedita itaque. Temporibus debitis amet doloremque qui quaerat cupiditate
+        exercitationem aliquam?</p>
+      <ul>
+        <li><strong>Surface :</strong> 80 m²</li>
+        <li><strong>Pièces :</strong> 3</li>
+        <li><strong>Chambres :</strong> 2</li>
+        <li><strong>Salle de bain :</strong> 1</li>
+        <li><strong>Meublé :</strong> Oui</li>
+      </ul>
+      <a href="#" class="btn btn-primary text-light me-2">Nous contacter</a>
+      <a href="#" class="btn btn-danger">Ajouter aux favoris</a>
+    </div>
+  </div>
+
+  <hr>
+
+  <div class="row">
+    <div class="col">
+      <h3>Localisation</h3>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi labore totam, numquam ullam accusantium
+        cum odit rem saepe accusamus expedita itaque.</p>
+    </div>
+  </div>
+</div>
