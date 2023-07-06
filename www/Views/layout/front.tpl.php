d2710ac37c415c199967e68f370d016ce29bc011
Sydney Nana Tchuigoua <sydneynt11@gmail.com>
2023-07-06 16:39:21 +0200

    feat(new-front-template): integration of new template

--- www/Views/layout/front.tpl.php
+++ www/Views/layout/front.tpl.php
@@ -1,78 +1,145 @@
 <!DOCTYPE html>
-<html>
+<html lang="fr">
+
 <head>
   <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
   <title>Moving House</title>
   <meta name="description" content="Moving House">
-  <link rel="stylesheet" href="../asset/css/style.css">
-  <link rel="stylesheet" href="../asset/css/bootstrap.css">
-  <link rel="shortcut icon" href="../../asset/images/logo.png">
-</head>
-<body>
-  <div class="fluid-container">
-  <nav class="navbar navbar-expand-lg navbar-dark bg-darker-blue">
-  <div class="container-fluid mx-5">
-  <a class="navbar-brand d-flex align-items-center" href="/">
-      <img src="../asset/images/logo.png" alt="Logo" width="75" height="75" class="d-inline-block align-top me-2">
-      <span class="fs-4 mb-0">Moving House</span>
-    </a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse justify-content-end text-center" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link text-white fs-6" href="/a-propos">À propos</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link text-white fs-6" href="/contact">Contact</a>
-        </li>
-        <?php if(isset($_SESSION['zfgh_login']['connected']) && $_SESSION['zfgh_login']['connected'] == true): ?>
-          <li class="nav-item">
-            <a class="nav-link text-white fs-6" href="/account-settings">Parametre du compte</a>
-          </li>
-        <?php endif; ?>
-        <?php if(isset($_SESSION['zfgh_login']['connected']) && $_SESSION['zfgh_login']['connected'] == true && ($_SESSION['zfgh_login']['status'] == 2 || $_SESSION['zfgh_login']['status'] == 3)): ?>
-          <li class="nav-item">
-            <a class="nav-link text-white fs-6" href="/back">Administration</a>
-          </li>
-        <?php endif; ?>
-        <li class="nav-item">
-          <?php if(isset($_SESSION['zfgh_login']['connected']) && $_SESSION['zfgh_login']['connected'] == true): ?>
-            <a class="nav-link text-white fs-6" href="/logout">Déconnexion</a>
-          <?php else: ?>
-            <a class="nav-link text-white fs-6" href="/login">Mon compte</a>
-          <?php endif; ?>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
+  <meta name="keywords" content="">
 
-  </div>
+  <!-- Old template -->
+  <!-- <link rel="stylesheet" href="../asset/css/style.css">
+  <link rel="stylesheet" href="../asset/css/bootstrap.css">
+  <link rel="shortcut icon" href="../../asset/images/logo.png"> -->
+
+  <!-- ################ New template ################## -->
+
+  <!-- Favicons -->
+  <link rel="shortcut icon" href="../../asset/images/logo.png">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" rel="stylesheet">
+
+  <!-- CSS Files -->
+  <link href="../../asset/front-template/animate.css/animate.min.css" rel="stylesheet">
+  <link href="../../asset/front-template/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="../../asset/front-template/bootstrap-icons/bootstrap-icons.min.css" rel="stylesheet">
+  <link href="../../asset/front-template/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Main style CSS -->
+  <link href="../../asset/front-template/style.css" rel="stylesheet">
+
+</head>
+
+<body>
+  <!-- BEGIN: Header -->
+  <!-- <div class="fluid-container">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-darker-blue">
+      <div class="container-fluid mx-5">
+        <a class="navbar-brand d-flex align-items-center" href="/">
+          <img src="../asset/images/logo.png" alt="Logo" width="75" height="75" class="d-inline-block align-top me-2">
+          <span class="fs-4 mb-0">Moving House</span>
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-end text-center" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link text-white fs-6" href="/a-propos">À propos</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-white fs-6" href="/contact">Contact</a>
+            </li>
+            <?php if(isset($_SESSION['zfgh_login']['connected']) && $_SESSION['zfgh_login']['connected'] == true): ?>
+              <li class="nav-item">
+                <a class="nav-link text-white fs-6" href="/account-settings">Parametre du compte</a>
+              </li>
+            <?php endif; ?>
+            <?php if(isset($_SESSION['zfgh_login']['connected']) && $_SESSION['zfgh_login']['connected'] == true && ($_SESSION['zfgh_login']['status'] == 2 || $_SESSION['zfgh_login']['status'] == 3)): ?>
+              <li class="nav-item">
+                <a class="nav-link text-white fs-6" href="/back">Administration</a>
+              </li>
+            <?php endif; ?>
+            <li class="nav-item">
+              <?php if(isset($_SESSION['zfgh_login']['connected']) && $_SESSION['zfgh_login']['connected'] == true): ?>
+                <a class="nav-link text-white fs-6" href="/logout">Déconnexion</a>
+              <?php else: ?>
+                <a class="nav-link text-white fs-6" href="/login">Mon compte</a>
+              <?php endif; ?>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+  </div> -->
+  <!-- END: Header -->
+
+
+
+  <!-- ======= Header/Navbar ======= -->
+  <nav class="navbar navbar-default navbar-trans navbar-expand-lg fixed-top">
+    <div class="container">
+      <button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarDefault" aria-controls="navbarDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span></span>
+        <span></span>
+        <span></span>
+      </button>
+      <a class="navbar-brand text-brand" href="/"><span class="color-b">Moving</span>House</a>
+
+      <div class="navbar-collapse collapse justify-content-center" id="navbarDefault">
+        <ul class="navbar-nav">
+
+          <li class="nav-item">
+            <a class="nav-link active" href="/">Accueil</a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link " href="/about-us">A propos</a>
+          </li>
+
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Proriétés</a>
+            <div class="dropdown-menu">
+              <a class="dropdown-item " href="#">Acheter</a>
+              <a class="dropdown-item " href="#">Louer</a>
+            </div>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link " href="/contact">Contact</a>
+          </li>
+          
+        </ul>
+      </div>
+
+      <a  href="/login" class="btn btn-b-n">
+        <i class="bi bi-person-square"></i>
+        <span class="space">Mon Compte</span>
+      </a>
+
+    </div>
+  </nav><!-- End Header/Navbar -->
 
   <!-- BEGIN : Main Content -->
-  <main class="container py-5">
+  <main class="container intro-single">
     {{content}}
   </main>
   <!-- END : Main Content -->
 
+
   <!-- BEGIN: Footer -->
-  <footer>
+  <!-- <footer>
     <div class="container-fluid pt-5 pb-3 bg-darker-blue">
       <section class="newsletter">
         <div class="container">
           <div class="row justify-content-center">
             <div class="col-md-8">
               <h2 class="text-center text-light mb-4">Inscrivez-vous à notre newsletter</h2>
-              <!-- <form class="row g-3">
-                <div class="col-sm-8">
-                  <input type="email" class="form-control" placeholder="Votre adresse e-mail" required>
-                </div>
-                <div class="col-sm-4">
-                  <button type="submit" class="btn btn-outline-light btn-block mb-4 w-100">S'inscrire</button>
-                </div>
-              </form> -->
               {{newsletter}}
             </div>
           </div>
@@ -84,11 +151,11 @@
       </div>
       <p class="text-light text-center">Copyright &copy; 2023 Moving House | All Rights Reserved.</p>
     </div>
-  </footer>
+  </footer> -->
   <!-- END: Footer -->
   
-  <!-- BEGIN: Modal -->
-  <div class="modal fade" id="mentionsLegales" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <!-- BEGIN: Modal: Legal mentions -->
+  <!-- <div class="modal fade" id="mentionsLegales" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable modal-lg">
       <div class="modal-content">
         <div class="modal-header">
@@ -159,12 +226,163 @@
         </div>
       </div>
     </div>
-  </div>
-  <!-- END: Modal -->
+  </div> -->
+  <!-- END: Modal: Legal mentions -->
+
+    <!-- ======= Footer ======= -->
+    <section class="section-footer">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-12 col-md-4">
+          <div class="widget-a">
+            <div class="w-header-a">
+              <h3 class="w-title-a text-brand">Moving House</h3>
+            </div>
+            <div class="w-body-a">
+              <p class="w-text-a color-text-a">
+                Enim minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip exea commodo consequat duis
+                sed aute irure.
+              </p>
+            </div>
+            <div class="w-footer-a">
+              <ul class="list-unstyled">
+                <li class="color-a">
+                  <span class="color-text-a">Email :</span> contact@example.com
+                </li>
+                <li class="color-a">
+                  <span class="color-text-a">Phone :</span> +54 356 945234
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-12 col-md-4 section-md-t3">
+          <div class="widget-a">
+            <div class="w-header-a">
+              <h3 class="w-title-a text-brand">La société</h3>
+            </div>
+            <div class="w-body-a">
+              <div class="w-body-a">
+                <ul class="list-unstyled">
+                  <li class="item-list-a">
+                    <i class="bi bi-chevron-right"></i> <a href="#">Site Map</a>
+                  </li>
+                  <li class="item-list-a">
+                    <i class="bi bi-chevron-right"></i> <a href="#">Mention légales</a>
+                  </li>
+                  <li class="item-list-a">
+                    <i class="bi bi-chevron-right"></i> <a href="#">Agent Admin</a>
+                  </li>
+                  <li class="item-list-a">
+                    <i class="bi bi-chevron-right"></i> <a href="#">Careers</a>
+                  </li>
+                  <li class="item-list-a">
+                    <i class="bi bi-chevron-right"></i> <a href="#">Affiliate</a>
+                  </li>
+                  <li class="item-list-a">
+                    <i class="bi bi-chevron-right"></i> <a href="#">Privacy Policy</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-12 col-md-4 section-md-t3">
+          <div class="widget-a">
+            <div class="w-header-a">
+              <h3 class="w-title-a text-brand">Propriétés</h3>
+            </div>
+            <div class="w-body-a">
+              <ul class="list-unstyled">
+                <li class="item-list-a">
+                  <i class="bi bi-chevron-right"></i> <a href="#">Appartement</a>
+                </li>
+                <li class="item-list-a">
+                  <i class="bi bi-chevron-right"></i> <a href="#">Maison</a>
+                </li>
+                <li class="item-list-a">
+                  <i class="bi bi-chevron-right"></i> <a href="#">Parking</a>
+                </li>
+                <li class="item-list-a">
+                  <i class="bi bi-chevron-right"></i> <a href="#">Terrain</a>
+                </li>
+                <li class="item-list-a">
+                  <i class="bi bi-chevron-right"></i> <a href="#">Singapore</a>
+                </li>
+                <li class="item-list-a">
+                  <i class="bi bi-chevron-right"></i> <a href="#">Philippines</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <footer>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <nav class="nav-footer">
+            <ul class="list-inline">
+              <li class="list-inline-item">
+                <a href="/">Accueil</a>
+              </li>
+              <li class="list-inline-item">
+                <a href="/about-us">A propos</a>
+              </li>
+              <li class="list-inline-item">
+                <a href="/contact">Contact</a>
+              </li>
+            </ul>
+          </nav>
+          <div class="socials-a">
+            <ul class="list-inline">
+              <li class="list-inline-item">
+                <a href="#">
+                  <i class="bi bi-facebook" aria-hidden="true"></i>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a href="#">
+                  <i class="bi bi-twitter" aria-hidden="true"></i>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a href="#">
+                  <i class="bi bi-instagram" aria-hidden="true"></i>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a href="#">
+                  <i class="bi bi-linkedin" aria-hidden="true"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div class="copyright-footer">
+            <p class="copyright color-text-a">
+              &copy; Copyright 2023
+              <span class="color-a">Moving House</span> | All Rights Reserved.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer><!-- End  Footer -->
 
   <!-- Script JS -->
-  <script src="../asset/js/bootstrap.js"></script>
-  <script src="../asset/js/jquery.js"></script>
+  <!-- <script src="../asset/js/bootstrap.js"></script>
+  <script src="../asset/js/jquery.js"></script> -->
+
+
+  <!-- Vendor JS Files -->
+  <script src="../../asset/front-template/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="../../asset/front-template/swiper/swiper-bundle.min.js"></script>
+  <script src="../../asset/front-template/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="../../asset/front-template/main.js"></script>
 
 </body>
 </html>
