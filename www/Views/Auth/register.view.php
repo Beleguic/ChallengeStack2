7c8a7e15c2c1c9d08056afbc71c61c4eecabf561
Sydney Nana Tchuigoua <sydneynt11@gmail.com>
2023-07-06 16:40:27 +0200

    feat(new-front-template): new template for login/register/activation forms

--- www/Views/Auth/register.view.php
+++ www/Views/Auth/register.view.php
@@ -1,9 +1,12 @@
-<h2 class="text-center title-form pt-5">Inscription</h2>
+<h2 class="text-center title-form mb-4">Inscription</h2>
 
 <?php $this->partial("form", $this->data["form"]) ?>
-<?php if(isset($this->data['formErrors'])): ?>
-	<?php print_r($this->data['formErrors']) ?>
-<?php endif; ?>
 
-<a href="/login">Se connecter</a>
-<a href="/">Accueil</a>
+<div class="bg-warning">
+	<p>Gestion des msg d'erreurs</p>
+	<?php if(isset($this->data['formErrors'])): ?>
+		<?php print_r($this->data['formErrors']) ?>
+	<?php endif; ?>
+</div>
+
+<p class="text-center">Vous avez déjà un compte ? <a href="/login" class="color-b">Se connecter</a></p>
