21478ba944a418942dc7ef6ebb8f7520be1fd2f2
Jason <jason.afonso.fernandes@gmail.com>
2023-07-09 23:15:26 +0200

    mise en place du nouveau react, sans router

--- www/index.php
+++ www/index.php
@@ -15,7 +15,7 @@
     use App\Controllers\Newsletter;
 
  if (!file_exists('config.php') && $_SERVER['REQUEST_URI'] !== '/api/installer') {
-        header('Location: /public/index.html');
+        header('Location: /public/react/index.html');
     exit;
     }
     
