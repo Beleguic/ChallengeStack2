b1f2ff0dbdb0ccff8f5f0e8d18f0e2a47c604504
Thibault Beleguic <thibault.beleguic2@gmail.com>
2023-07-20 19:38:05 +0200

    fix ini and table name prefix

--- www/Models/Agent.php
+++ www/Models/Agent.php
@@ -24,7 +24,7 @@ class Agent extends SQL implements SQLInterface
         $sql = parent::getInstance();
         $classExploded = explode("\\", get_called_class());
         $this->pdo = $sql->pdo;
-        $this->table = "zfgh_".end($classExploded);
+        $this->table = "".$GLOBALS['prefixe']."_".end($classExploded);
     }
 
     public function getConfigObject(): array
