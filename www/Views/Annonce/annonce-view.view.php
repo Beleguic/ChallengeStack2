92942ac1ec7f940e2dd1fe479f3a398c46b63b49
Thibault Beleguic <thibault.beleguic2@gmail.com>
2023-07-21 15:31:18 +0200

    fix crud annnonce

--- www/Views/Annonce/annonce-view.view.php
+++ www/Views/Annonce/annonce-view.view.php
@@ -66,20 +66,8 @@
 						<th> Titre </th>
 						<th> Prix </th>
 						<th> Type </th>
-						<th> Superficie Maison </th>
-						<th> Superficie Terrain </th>
-						<th> Nombre de pieces </th>
-						<th> Nombre de chambre </th>
 						<th> Ville </th>
-						<th> Rue </th>
-						<th> Description </th>
 						<th> Adresse complete </th>
-						<th> Code postale </th>
-						<th> N° Département </th>
-						<th> Département </th>
-						<th> postale </th>
-						<th> Latitude </th>
-						<th> Longitude </th>
 				</thead>
 				<tbody>	
 			<?php while ($row = $this->data['annonceList']->fetch()): ?>
@@ -87,19 +75,8 @@
 						<td><?=$row->getTitre()?></td>
 						<td><?=$row->getPrix()?></td>
 						<td><?=$row->getTexte()?></td>
-						<td><?=$row->getSuperficiemaison()?></td>
-						<td><?=$row->getSuperficieterrain()?></td>
-						<td><?=$row->getNbrpiece()?></td>
-						<td><?=$row->getNbrchambre()?></td>
-						<td><?=$row->getDescription()?></td>
 						<td><?=$row->getCity()?></td>
 						<td><?=$row->getAddressComplet()?></td>
-						<td><?=$row->getPostCode()?></td>
-						<td><?=$row->getDepNum()?></td>
-						<td><?=$row->getDepLabel()?></td>
-						<td><?=$row->getRegions()?></td>
-						<td><?=$row->getLatitude()?></td>
-						<td><?=$row->getLongitude()?></td>
 						<td>
 							<a class="button-Update" href="/back/update-annonce?id=<?=$row->getId()?>">Modifier</a>
 							<a class="button-Delete" href="/back/delete-annonce?id=<?=$row->getId()?>">Supprimer</a>
